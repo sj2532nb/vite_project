22ede8c94d0c8f88507e2a733483c6fe7bbc23cc
sj2532nb <sj2532nb@gmail.com>
2024-03-30 22:50:46 +0900

    subak_game

---
+++ planets.js
@@ -0,0 +1,42 @@
+export const PLANETS = [
+    {
+        name: "00_star",
+        radius: 30 / 2,
+    },
+    {
+        name: "01_moon",
+        radius: 60 / 2,
+    },
+    {
+        name: "02_venus",
+        radius: 90 / 2,
+    },
+    {
+        name: "03_earth",
+        radius: 121 / 2,
+    },
+    {
+        name: "04_mars",
+        radius: 150 / 2,
+    },
+    {
+        name: "05_jupiter",
+        radius: 187 / 2,
+    },
+    // {
+    //     name: "06_saturn",
+    //     radius: 123 / 2,
+    // },
+    {
+        name: "07_uranus",
+        radius: 210 / 2,
+    },
+    {
+        name: "08_neptune",
+        radius: 240 / 2,
+    },
+    {
+        name: "09_sun",
+        radius: 270 / 2,
+    }
+];
